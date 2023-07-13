053f61ade322b3b5e733ee0006d6eff0c9d7fd19
Moritz Baumgart <baumgartmoritz@web.de>
2023-07-13 21:22:08 +0200

    accidentally commited only half of the files in prev commit

---
+++ PostlyApp/ViewModels/PostViewViewModel.cs
@@ -0,0 +1,59 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using PostlyApp.Enums;
+using PostlyApp.Models.DTOs;
+using System.ComponentModel;
+
+namespace PostlyApp.ViewModels
+{
+    internal partial class PostViewViewModel : ObservableObject
+    {
+
+        [ObservableProperty]
+        private PostDTO _post;
+
+        [ObservableProperty]
+        private ImageSource _upvoteImg;
+
+        [ObservableProperty]
+        private ImageSource _downvoteImg;
+
+
+
+        public PostViewViewModel(PostDTO post)
+        {
+            Post = post;
+            post.PropertyChanged += OnVoteChange;
+            OnVoteChange(post.Vote);
+        }
+
+        private void OnVoteChange(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(PostDTO.Vote))
+            {
+                OnVoteChange(Post.Vote);
+            }
+        }
+
+        private void OnVoteChange(VoteType? vote)
+        {
+            if (vote != null)
+            {
+                if (vote == VoteType.Upvote)
+                {
+                    UpvoteImg = "thumbs_up_solid.png";
+                    DownvoteImg = "thumbs_down_regular.png";
+                }
+                else if (vote == VoteType.Downvote)
+                {
+                    UpvoteImg = "thumbs_up_regular.png";
+                    DownvoteImg = "thumbs_down_solid.png";
+                }
+            }
+            else
+            {
+                UpvoteImg = "thumbs_up_regular.png";
+                DownvoteImg = "thumbs_down_regular.png";
+            }
+        }
+    }
+}
